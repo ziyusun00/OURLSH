ec265cc57253a1ae9486a7a51d23c3526f4e07fe
wesoa012 <wesa@smu.edu>
2022-10-20 09:34:42 -0500

    SQL syntax update. RDS is now live with database installed

--- backend/mysql_setup.sql
+++ backend/mysql_setup.sql
@@ -2,76 +2,65 @@ CREATE DATABASE IF NOT EXISTS ourlsh;
 
 USE ourlsh;
 
-    CREATE TABLE 'ourlsh'.'landlord'(
-        'id' SERIAL NOT NULL,
-        'password' VARCHAR(255) NOT NULL,
-        'email' VARCHAR(255) NOT NULL,
-        'first_name' VARCHAR(255) NOT NULL,
-        'last_name' VARCHAR(255) NOT NULL,
-         PRIMARY KEY ('id')
-    );
+# LANDLORD TABLE
+CREATE TABLE landlord(
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        password VARCHAR(255) NOT NULL,
+        email VARCHAR(255) NOT NULL,
+        first_name VARCHAR(255) NOT NULL,
+        last_name VARCHAR(255) NOT NULL
+);
 
-     CREATE TABLE 'ourlsh'.'prop'(
-        'id' SERIAL NOT NULL,
-        'date_available' DATE NOT NULL,
-        'status' INT  NOT NULL,
-        'landlord_id' INT  NOT NULL,
-        'prop_name' VARCHAR(255) NOT NULL,
-        'address' VARCHAR(255) NOT NULL,
-        'description' VARCHAR (255) NOT NULL,
-        'occupied' BOOLEAN NOT NULL,
-        PRIMARY KEY ('id'),
-        FOREIGN KEY ('landlord_id') REFERENCES landlord(id)
+# PROPERTY TABLE
+CREATE TABLE prop(
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        date_available DATE NOT NULL,
+        status INT NOT NULL,
+        landlord_id INT  NOT NULL REFERENCES landlord(id),
+        prop_name VARCHAR(255) NOT NULL,
+        address VARCHAR(255) NOT NULL,
+        description VARCHAR (255) NOT NULL,
+        occupied BOOLEAN NOT NULL
+);
 
-        
-    );
+# TENANT TABLE
+CREATE TABLE tenant(
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        password VARCHAR(255) NOT NULL,
+        email VARCHAR(255) NOT NULL,
+        first_name VARCHAR(255) NOT NULL,
+        last_name VARCHAR(255) NOT NULL,
+        prop_id INT NOT NULL REFERENCES prop(id),
+        landlord_id INT NOT NULL REFERENCES landlord(id)
+);
 
-    CREATE TABLE 'ourlsh'.'tenant'(
-        'id' SERIAL NOT NULL,
-        'password' VARCHAR(255) NOT NULL,
-        'email' VARCHAR(255) NOT NULL,
-        'first_name' VARCHAR(255) NOT NULL,
-        'last_name' VARCHAR(255) NOT NULL,
-        'prop_id' INT NOT NULL,
-        'landlord_id' INT NOT NULL,
-        PRIMARY KEY ('id'),
-        FOREIGN KEY ('landlord_id') REFERENCES landlord(id),
-        FOREIGN KEY ('prop_id') REFERENCES 'ourlsh'.'prop'(id)
+# INVOICE TABLE
+CREATE TABLE invoice(
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        amount FLOAT NOT NULL,
+        date VARCHAR(255) NOT NULL,
+        payment_type VARCHAR(255) NOT NULL,
+        what_for INT NOT NULL,
+        land_id INT NOT NULL REFERENCES landlord(id),
+        prop_id INT NOT NULL REFERENCES prop(id),
+        tenant_id INT NOT NULL REFERENCES tenant(id)
+);
 
-    );
+# WORK ORDER TABLE
+CREATE TABLE work_order(
+        wo_num INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        description VARCHAR(255) NOT NULL,
+        status INT NOT NULL,
+        date VARCHAR(255) NOT NULL,
+        resolved BOOLEAN NOT NULL,
+        importance INT NOT NULL,
+        tenant_id INT NOT NULL REFERENCES tenant(id),
+        prop_id INT NOT NULL REFERENCES prop(id),
+        invoice_id INT NOT NULL REFERENCES invoice(id),
+        land_id INT NOT NULL REFERENCES landlord(id)
+);
 
-    CREATE TABLE 'ourlsh'.'work_order'(
-        'wo_num' SERIAL NOT NULL,
-        'prop_id' INT NOT NULL,
-        'status' INT NOT NULL,
-        'date' VARCHAR(255) NOT NULL,
-        'resolved' BOOLEAN NOT NULL,
-        'importance' INT NOT NULL,
-        'tenant_id' INT NOT NULL,
-        'description' VARCHAR(255) NOT NULL,
-        'invoice_id' INT NOT NULL,
-        'land_id' INT NOT NULL,
-        PRIMARY KEY ('wo_num'),
-        FOREIGN KEY ('prop_id') REFERENCES 'ourlsh'.'prop'(id),
-        FOREIGN KEY ('tenant_id') REFERENCES 'ourlsh'.'tenant'(id),
-        FOREIGN KEY ('invoice_id') REFERENCES 'ourlsh'.'invoice'(id),
-        FOREIGN KEY ('land_id') REFERENCES 'ourlsh'.'landlord'(id)
 
-    )
-
-    CREATE TABLE 'ourlsh'.'invoice'(
-        'id' SERIAL NOT NULL,
-        'amount' FLOAT NOT NULL,
-        'date' VARCHAR(255) NOT NULL,
-        'payment_type' VARCHAR(255) NOT NULL,
-        'for' INT NOT NULL,
-        'land_id' INT NOT NULL,
-        'prop_id' INT NOT NULL,
-        'tenant_id' INT NOT NULL,
-        FOREIGN KEY ('prop_id') REFERENCES 'ourlsh'.'prop'(id),
-        FOREIGN KEY ('tenant_id') REFERENCES 'ourlsh'.'tenant'(id),
-        FOREIGN KEY ('land_id') REFERENCES 'ourlsh'.'landlord'(id)
-    )
 
    
 
