3b8091f4c1007186f3ad5373c2a427537f1607da
MuhAsh99 <mjashraf@smu.edu>
2022-10-19 19:35:16 -0500

    Update mysql_setup.sql
    creating tables

--- backend/mysql_setup.sql
+++ backend/mysql_setup.sql
@@ -1,18 +1,26 @@
--- create database db
-CREATE DATABASE db;
+CREATE DATABASE IF NOT EXISTS ourlsh;
+    
+USE ourlsh;
 
--- use newly create database
-USE db;
+    CREATE TABLE 'ourlsh'.'landlord'(
+        'id' SERIAL,
+        'password' VARCHAR(255) NOT NULL,
+        'email' VARCHAR(255) NOT NULL,
+        'first_name' VARCHAR(255) NOT NULL, 
+        'last_name' VARCHAR(255) NOT NULL,
+         PRIMARY KEY ('id')
+    );
 
--- create table in db
-CREATE TABLE `db`.`test_table` (
-    `id` INT NOT NULL AUTO_INCREMENT, 
-    `value` VARCHAR(45), 
-    PRIMARY KEY (`id`), 
-    UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE
-);
-
--- insert sample entry
-INSERT INTO `db`.`test_table` (`value`) VALUES ('Sample Value');
-
-
+    CREATE TABLE 'ourlsh'.'tenant'(
+        'id' SERIAL,
+        'password' VARCHAR(255) NOT NULL,
+        'email' VARCHAR(255) NOT NULL,
+        'first_name' VARCHAR(255) NOT NULL, 
+        'last_name' VARCHAR(255) NOT NULL,
+        'prop_id' INT, 
+        'landlord_id' INT,
+        PRIMARY KEY ('id'),
+        FOREIGN KEY ('landlord_id') REFERENCES landlord(id),
+        
+        CREATE TABLE 'oprop
+    );
