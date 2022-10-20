b34f111209a226656808242d4e4bd363d3718ac9
MuhAsh99 <mjashraf@smu.edu>
2022-10-19 19:36:36 -0500

    Update mysql_setup.sql
    adding tables

--- backend/mysql_setup.sql
+++ backend/mysql_setup.sql
@@ -1,26 +1,26 @@
 CREATE DATABASE IF NOT EXISTS ourlsh;
-    
+
 USE ourlsh;
 
-    CREATE TABLE 'ourlsh'.'landlord'(
+    CREATE TABLE landlord(
         'id' SERIAL,
         'password' VARCHAR(255) NOT NULL,
         'email' VARCHAR(255) NOT NULL,
-        'first_name' VARCHAR(255) NOT NULL, 
+        'first_name' VARCHAR(255) NOT NULL,
         'last_name' VARCHAR(255) NOT NULL,
          PRIMARY KEY ('id')
     );
 
-    CREATE TABLE 'ourlsh'.'tenant'(
+    CREATE TABLE tenant(
         'id' SERIAL,
         'password' VARCHAR(255) NOT NULL,
         'email' VARCHAR(255) NOT NULL,
-        'first_name' VARCHAR(255) NOT NULL, 
+        'first_name' VARCHAR(255) NOT NULL,
         'last_name' VARCHAR(255) NOT NULL,
-        'prop_id' INT, 
+        'prop_id' INT,
         'landlord_id' INT,
         PRIMARY KEY ('id'),
         FOREIGN KEY ('landlord_id') REFERENCES landlord(id),
-        
-        CREATE TABLE 'oprop
+
+        CREATE TABLE prop
     );
