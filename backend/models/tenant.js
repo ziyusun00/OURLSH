d033ecb9a61ac26be650d7607ba0fb90d40dfc21
wesoa012 <wesa@smu.edu>
2022-10-26 13:59:27 -0500

    Removed console testing

--- backend/models/tenant.js
+++ backend/models/tenant.js
@@ -2,7 +2,6 @@ const knex = require('../database/knex');
 const TENANT_TABLE = 'tenant';
    
    const fetchTenantByID = async (id) => {
-      console.log("made it inside the models/tenant.js")
       //uses passed in id to get the associated tenant but only the specified columns
        const query = knex(TENANT_TABLE).where({ id })
        .select("id",
