e65c14a18c367c9563f8b2c7b6eb4e181b0a21b0
MuhAsh99 <mjashraf@smu.edu>
2022-10-26 14:28:55 -0500

    Update register.js
    added salting and hashing to landlord

--- backend/models/register.js
+++ backend/models/register.js
@@ -17,8 +17,14 @@ const bcrypt = require('bcrypt');
    }
 
    const createLandlord = async (email, password, first_name, last_name) => {
+      console.log("raw password: ", password);
+      const salt = await bcrypt.genSalt(10);
+      console.log("salted password: ", salt);
+      const hashedPassword = await bcrypt.hash(password, salt);
+      console.log('Hashed password', hashedPassword);
+
       console.log("creating a landlord")
-      const query = knex(LANDLORD_TABLE).insert({email, password, first_name, last_name});
+      const query = knex(LANDLORD_TABLE).insert({email, password: hashedPassword, first_name, last_name});
       const results = await query;
       return results;
    }
