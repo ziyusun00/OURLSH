b19dd8c762093d45d5eb6cf982208de6751cd9df
MuhAsh99 <mjashraf@smu.edu>
2022-10-26 14:27:32 -0500

    Update register.js
    added hashing and salting to password

--- backend/models/register.js
+++ backend/models/register.js
@@ -1,10 +1,17 @@
 const knex = require('../database/knex');
 const TENANT_TABLE = 'tenant';
 const LANDLORD_TABLE = 'landlord';
+const bcrypt = require('bcrypt');
    
    const createTenant = async (email, password, first_name, last_name) => {
+      console.log("raw password: ", password);
+      const salt = await bcrypt.genSalt(10);
+      console.log("salted password: ", salt);
+      const hashedPassword = await bcrypt.hash(password, salt);
+      console.log('Hashed password', hashedPassword);
+
       console.log("creating a tenant")
-      const query = knex(TENANT_TABLE).insert({email, password, first_name, last_name});
+      const query = knex(TENANT_TABLE).insert({email, password: hashedPassword, first_name, last_name});
       const results = await query;
       return results;
    }
