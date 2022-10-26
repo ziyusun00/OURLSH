7799fede3705ee6269c93401333bcad27edeb946
wesoa012 <wesa@smu.edu>
2022-10-26 14:01:43 -0500

    Added some checks to the get by id request
    Still need to add in Authentication to make sure user has access to get a tenant

--- backend/routes/tenant.js
+++ backend/routes/tenant.js
@@ -1,3 +1,4 @@
+const { json } = require('body-parser');
 const bodyParser = require('body-parser');
 const express = require('express');
 router = express.Router();
@@ -6,12 +7,26 @@ router.use(bodyParser.json());
 
 
 router.get('/:id', async (req, res, next) => {
-    console.log("made it in the routes/tenant.js")
     let accountId = parseInt(req.params.id);
     //let accountId = req.query.id; //Don't know whether params or body will be used
-     
-    const tenantByID = await req.models.tenant.fetchTenantByID(accountId);
-    res.json(tenantByID);
+    if(typeof(accountId) !== Number)
+    {
+        console.log("account_id is not type Number")
+        res.status(400).send();
+    }
+    else{
+        const tenantByID = await req.models.tenant.fetchTenantByID(accountId);
+        //will return 404 not found if id does not exist
+        if(JSON.stringify(tenantByID) == '[]')
+        {
+            console.log("No user found with id", accountId)
+            res.status(404).send();
+        }
+        else
+        {
+            res.json(tenantByID);
+        }
+    }
     next();
 });
 
