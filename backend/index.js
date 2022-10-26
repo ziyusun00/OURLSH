4bb25588a10e1a6c1593ec34631ffe4b6752ba28
wesoa012 <wesa@smu.edu>
2022-10-26 13:59:56 -0500

    now using register routes

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
-const tenantRoutes = require('./routes/tenant' );
-const {createModelsMiddleware} = require('./middleware/model-middleware' );
+const tenantRoutes = require('./routes/tenant');
+const { createModelsMiddleware } = require('./middleware/model-middleware');
 const cors = require('cors');
 const app = express();
 const port = 8000;
@@ -10,15 +10,20 @@ app.use(createModelsMiddleware);
 //get rid of CORS issue
 app.use(cors({
     origin: '*'
-  }));
+}));
+
 app.get('/health', (request, response, next) => {
-   const responseBody = { status: 'up', port };
-   response.json(responseBody);
-   // next() is how we tell express to continue through the middleware chain
-   next();
+    const responseBody = { status: 'up', port };
+    response.json(responseBody);
+    // next() is how we tell express to continue through the middleware chain
+    next();
 });
+//tenants routes
 app.use('/tenants', tenantRoutes);
 
+//register routes
+app.use('/register', registerRoutes);
+
 app.listen(port, () => {
-   console.log(`This app is listening on port  ${port}`);
+    console.log(`This app is listening on port ${port}`);
 });
