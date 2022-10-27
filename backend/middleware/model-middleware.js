64aa12751ab11870a13e2f75fbc5ad7ca8181eca
wesoa012 <wesa@smu.edu>
2022-10-26 19:01:32 -0500

    new login model included

--- backend/middleware/model-middleware.js
+++ backend/middleware/model-middleware.js
@@ -1,11 +1,13 @@
 const Tenant = require('../models/tenant');
 const Register = require('../models/register');
 const Landlord = require('../models/landlord');
+const Login = require('../models/login');
 const createModelsMiddleware = async (req, res, next) => {
    req.models = {
       tenant: Tenant,
       register: Register,
-      landlord: Landlord
+      landlord: Landlord,
+      login: Login
   }
   next();
 }
